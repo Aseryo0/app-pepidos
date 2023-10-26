03ab5a0bc1ba59b5427ea6d05fe26b7c69bcde98
Aseryo0 <andrefelipeil@hotmail.com>
2023-10-26 02:09:20 -0300

    ciado logica de adicionar pedidos em quantidade

--- src/components/Snakcks/index.tsx
+++ src/components/Snakcks/index.tsx
@@ -9,24 +9,30 @@ interface SnacksProps {
 }
 
 export const Snakcs = ({ snakcs }: SnacksProps) => {
-  const { addSnackIntoCart } = useCart()
+  const { cart, addSnackIntoCart } = useCart()
   return (
     <Container>
       {!snakcs.length
         ? [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
-        : snakcs.map((snack) => (
-            <div key={snack.id} className='snack'>
-              <h2>{snack.name}</h2>
-              <img src={snack.image} alt={snack.name} />
-              <p>{snack.description}</p>
-              <div>
-                <strong>{CurrencyFormat(snack.price)}</strong>
-                <button type='button' onClick={() => addSnackIntoCart(snack)}>
-                  +
-                </button>
+        : snakcs.map((snack) => {
+            const snackExist = cart.find(
+              (item) => item.snack === snack.snack && item.id === snack.id,
+            )
+            return (
+              <div key={snack.id} className='snack'>
+                {snackExist && <span>{snackExist.quantity}</span>}
+                <h2>{snack.name}</h2>
+                <img src={snack.image} alt={snack.name} />
+                <p>{snack.description}</p>
+                <div>
+                  <strong>{CurrencyFormat(snack.price)}</strong>
+                  <button type='button' onClick={() => addSnackIntoCart(snack)}>
+                    +
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          })}
     </Container>
   )
 }
